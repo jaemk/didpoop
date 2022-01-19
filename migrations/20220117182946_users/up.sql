667d9b2141720e6657b869a466cd382d64539169
James Kominick <james@kominick.com>
2022-01-18 21:06:39 -0500

    formatting

--- migrations/20220117182946_users/up.sql
+++ migrations/20220117182946_users/up.sql
@@ -3,9 +3,9 @@ begin;
         id       bigint primary key default poop.id_gen(),
         email    text unique not null,
         name     text not null,
-        pw_salt    text not null,
-        pw_hash    text not null,
-        deleted boolean not null default false,
+        pw_salt  text not null,
+        pw_hash  text not null,
+        deleted  boolean not null default false,
         created  timestamptz not null default now(),
         modified timestamptz not null default now()
     );
@@ -13,12 +13,12 @@ begin;
         where deleted is false;
 
     create table poop.auth_tokens (
-        id bigint primary key default poop.id_gen(),
-        user_id bigint not null references poop.users(id) on delete cascade,
-        hash text unique not null,
-        expires timestamptz not null,
-        deleted boolean not null default false,
-        created timestamptz not null default now(),
+        id       bigint primary key default poop.id_gen(),
+        user_id  bigint not null references poop.users(id) on delete cascade,
+        hash     text unique not null,
+        expires  timestamptz not null,
+        deleted  boolean not null default false,
+        created  timestamptz not null default now(),
         modified timestamptz not null default now()
     );
     create index auth_tokens_user_id on poop.auth_tokens(user_id)
